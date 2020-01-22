68b486d14ab02b19884c2a3ba3574432b0f11a15
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2020-01-22 17:19:06 +0100

    reglage acces cors

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Première page</h1>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    apiResponse: [],
+    data: []
+  }
+  
+  
+  componentDidMount = () => {
+    axios.get('http://localhost:8080/train-react-php/back-office/api.php')
+    .then(response => {
+      this.setState( { apiResponse: response.data } )
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  render(){
+    return (
+      <div className="box">
+        
+      </div>
+    );
+  }
 }
-
 export default App;
+
